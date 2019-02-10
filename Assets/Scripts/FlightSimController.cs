edf8a57450de63d6b4a9d9c647b0571834c45ef1
Shannon <shannonke@gatech.edu>
2019-02-10 02:14:02 -0500

    moving shit around

--- Assets/Scripts/FlightSimController.cs
+++ Assets/Scripts/FlightSimController.cs
@@ -10,23 +10,43 @@ using System.IO;
 
 public class FlightSimController : MonoBehaviour
 {
+	//for flight controller
 	public Text coords, flightnum, location, planedata, time, airbus, warning;
 	public CanvasGroup alert;
 	public Image weather;
-	public GameObject plane, altitude, airspeed;
+	public GameObject holo, planebody, plane, altitude, airspeed;
 	public Material green;
 	public Material red;
 	public TextAsset data;
+
+	//for main menu
 
 	public const string connectionString = "mongodb://admin:Dasani112@ds145704.mlab.com:45704/aerovizdb";
     public const int Max = 100;
 	public const int WaitTime = 3;
 
+	private MongoClient client;
+	private MongoServer server;
+	private MongoDatabase database;
+	private string collectionName;
+
+	
 	void Start() {
-		MongoClient client = new MongoClient(connectionString); 
-        MongoServer server = client.GetServer();
-        MongoDatabase database = server.GetDatabase("aerovizdb");
-        string collectionName = "flight1as";
+		client = new MongoClient(connectionString); 
+        server = client.GetServer();
+        database = server.GetDatabase("aerovizdb");
+
+	}
+
+	void Update() {
+		//update collectionName here based on user input
+	}
+
+	public void Simulation() {
+		//z = [1.8,3.2]
+		//x = [-1.77, 1.3]
+
+		collectionName = "flight1as";
 
 		MongoCollection collection = database.GetCollection(collectionName);
 		//the following doesn't work bc distinct returns a generic set for some reason
@@ -34,9 +54,6 @@ public class FlightSimController : MonoBehaviour
 		foreach (int i in collection.Distinct<int>("Offset_sec")) {
 			Debug.Log(i);
 		}
-	}
-
-	public void Simulation() {
 		////how to update weather sprite
 		//weather.sprite = (Sprite)Resources.Load<Sprite>("weather/" + {nameOfpng.png}) as Sprite;
 
