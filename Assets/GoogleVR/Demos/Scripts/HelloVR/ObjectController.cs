d6045e891ea1a6182f3754ac23154e1148d1498d
Shannon <shannonke@gatech.edu>
2019-02-09 19:05:58 -0500

    worked on hologram stuff

--- Assets/GoogleVR/Demos/Scripts/HelloVR/ObjectController.cs
+++ Assets/GoogleVR/Demos/Scripts/HelloVR/ObjectController.cs
@@ -20,93 +20,94 @@ namespace GoogleVR.HelloVR
 {
     using UnityEngine;
     using UnityEngine.EventSystems;
+    using UnityEngine.UI;
 
     [RequireComponent(typeof(Collider))]
     public class ObjectController : MonoBehaviour
     {
-        private Vector3 startingPosition;
-        private Renderer myRenderer;
+        // private Vector3 startingPosition;
+        // private Renderer myRenderer;
 
-        public Material inactiveMaterial;
-        public Material gazedAtMaterial;
-
+        
+        public CanvasGroup gazedAtMaterial;
+        
         void Start()
         {
-            startingPosition = transform.localPosition;
-            myRenderer = GetComponent<Renderer>();
+            //myRenderer = GetComponent<Renderer>();
             SetGazedAt(false);
         }
 
         public void SetGazedAt(bool gazedAt)
         {
-            if (inactiveMaterial != null && gazedAtMaterial != null)
-            {
-                myRenderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
-                return;
+            if (gazedAt) {
+                gazedAtMaterial.alpha = 0.75f;
+            } else {
+                gazedAtMaterial.alpha = 0.0f;
             }
         }
 
-        public void Reset()
-        {
-            int sibIdx = transform.GetSiblingIndex();
-            int numSibs = transform.parent.childCount;
-            for (int i = 0; i < numSibs; i++)
-            {
-                GameObject sib = transform.parent.GetChild(i).gameObject;
-                sib.transform.localPosition = startingPosition;
-                sib.SetActive(i == sibIdx);
-            }
-        }
+//         public void Reset()
+//         {
+//             int sibIdx = transform.GetSiblingIndex();
+//             int numSibs = transform.parent.childCount;
+//             for (int i = 0; i < numSibs; i++)
+//             {
+//                 GameObject sib = transform.parent.GetChild(i).gameObject;
+//                 sib.transform.localPosition = startingPosition;
+//                 sib.SetActive(i == sibIdx);
+//             }
+//         }
 
-        public void Recenter()
-        {
-#if !UNITY_EDITOR
-            GvrCardboardHelpers.Recenter();
-#else
-            if (GvrEditorEmulator.Instance != null)
-            {
-                GvrEditorEmulator.Instance.Recenter();
-            }
-#endif  // !UNITY_EDITOR
-        }
+//         public void Recenter()
+//         {
+// #if !UNITY_EDITOR
+//             GvrCardboardHelpers.Recenter();
+// #else
+//             if (GvrEditorEmulator.Instance != null)
+//             {
+//                 GvrEditorEmulator.Instance.Recenter();
+//             }
+// #endif  // !UNITY_EDITOR
+//         }
 
-        public void TeleportRandomly(BaseEventData eventData)
-        {
-            // Only trigger on left input button, which maps to
-            // Daydream controller TouchPadButton and Trigger buttons.
-            PointerEventData ped = eventData as PointerEventData;
-            if (ped != null)
-            {
-                if (ped.button != PointerEventData.InputButton.Left)
-                {
-                    return;
-                }
-            }
+//         public void TeleportRandomly(BaseEventData eventData)
+//         {
+//             // Only trigger on left input button, which maps to
+//             // Daydream controller TouchPadButton and Trigger buttons.
+//             PointerEventData ped = eventData as PointerEventData;
+//             if (ped != null)
+//             {
+//                 if (ped.button != PointerEventData.InputButton.Left)
+//                 {
+//                     return;
+//                 }
+//             }
 
-            // Pick a random sibling, move them somewhere random, activate them,
-            // deactivate ourself.
-            int sibIdx = transform.GetSiblingIndex();
-            int numSibs = transform.parent.childCount;
-            sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
-            GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
+//             // Pick a random sibling, move them somewhere random, activate them,
+//             // deactivate ourself.
+//             int sibIdx = transform.GetSiblingIndex();
+//             int numSibs = transform.parent.childCount;
+//             sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
+//             GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
 
-            // Move to random new location ±90˚ horzontal.
-            Vector3 direction = Quaternion.Euler(
-                0,
-                Random.Range(-90, 90),
-                0) * Vector3.forward;
+//             // Move to random new location ±90˚ horzontal.
+//             Vector3 direction = Quaternion.Euler(
+//                 0,
+//                 Random.Range(-90, 90),
+//                 0) * Vector3.forward;
 
-            // New location between 1.5m and 3.5m.
-            float distance = 2 * Random.value + 1.5f;
-            Vector3 newPos = direction * distance;
+//             // New location between 1.5m and 3.5m.
+//             float distance = 2 * Random.value + 1.5f;
+//             Vector3 newPos = direction * distance;
 
-            // Limit vertical position to be fully in the room.
-            newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
-            randomSib.transform.localPosition = newPos;
+//             // Limit vertical position to be fully in the room.
+//             newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
+//             randomSib.transform.localPosition = newPos;
 
-            randomSib.SetActive(true);
-            gameObject.SetActive(false);
-            SetGazedAt(false);
-        }
-    }
+//             randomSib.SetActive(true);
+//             gameObject.SetActive(false);
+//             SetGazedAt(false);
+//         }
+//     }
+}
 }
